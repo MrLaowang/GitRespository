d0398d278bbd746a23677f00e696f8776aa273c7
MrLaowang <975380810@qq.com>
2018-03-22 10:45:04 +0800

    解决冲突

--- aa/src/main/java/a/b/c/TestDate.java
+++ aa/src/main/java/a/b/c/TestDate.java
@@ -5,11 +5,10 @@ import org.apache.commons.lang3.time.DateUtils;
 public class TestDate {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("呵呵");
-=======
-	
+
 		System.out.println("哈哈");
->>>>>>> branch 'master' of https://github.com/MrLaowang/GitRespository.git
+
 	}
 }
